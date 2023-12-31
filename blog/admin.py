927c8a2725e55dc449918caf53d7c65e46375863
kirt <kirtwedel@yahoo.com>
2023-12-31 18:05:34 -0500

    nav-bar

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Blog
 
 # Register your models here.
+# Register your models here.
+admin.site.register(Blog)  # This will make our model visible on the admin page
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Blog
 
 
 # Create your views here.
 def all_blogs(request):
-    return render(request, "blog/all_blogs.html")
+    Blogs = Blog.objects.order_by("-date")  # -date is for descending order
+    return render(request, "blog/all_blogs.html", {"blogs": Blogs})
+
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)  # pk is primary key
+    return render(request, "blog/detail.html", {"blog": blog})
