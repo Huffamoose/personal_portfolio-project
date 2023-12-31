69ea6068114c85b131da7b7fa0d32ddf9145364b
kirt <kirtwedel@yahoo.com>
2023-12-30 21:52:36 -0500

    Models and Database

---
+++ portfolio/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Project
+
+
+# Create your views here.
+def home(request):
+    Projects = Project.objects.all()
+    return render(request, "portfolio/home.html", {"projects": Projects})
